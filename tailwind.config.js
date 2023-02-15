8874d8a5769cf3f45ac3341f4a3109b24278b57b
dgmick <lutmickael@gmail.com>
2023-02-15 19:59:15 +0100

    created profile infos

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,27 @@ module.exports = {
     './templates/**/*.html.twig',
   ],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+        padding: '1rem',
+        screens: {
+          sm: '600px',
+          md: '728px',
+          lg: '984px',
+          xl: '1240px',
+        },
+      },
+      colors: {
+        'color-white': '#f8f8f8',
+        'color-black': '#000',
+        'color-yellow': '#ffdb6e',
+        'color-green': '#7dba5c',
+        'color-gray': '#434242',
+        'color-dark-gray': '#121212',
+        'color-light-gray': '#4f4f4f',
+      }
+    },
   },
   plugins: [],
 }
