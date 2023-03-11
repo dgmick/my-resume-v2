7ca209036c1e71468c4f6231d4dd6b6661e9cdea
dgmick <lutmickael@gmail.com>
2023-03-11 22:28:50 +0100

    upload file

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\ImportPdfType;
 use App\Form\UserType;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -14,10 +16,30 @@ use Symfony\Component\HttpFoundation\Request;
 class AdminController extends AbstractController
 {
     #[Route(path: '/', name: 'app_admin')]
-    public function login(): Response
+    public function login(Request $request): Response
     {
+        $formPdf = $this->createForm(ImportPdfType::class);
+        $formPdf->handleRequest($request);
+
+        if ($formPdf->isSubmitted() && $formPdf->isValid()) {
+            /** @var UploadedFile $pdfFile */
+            $pdfFile = $formPdf->get('pdfFile')->getData();
+
+            $file_path = $this->getParameter('file_path');
+
+            $pdfFile->move(
+                $file_path,
+                $pdfFile->getClientOriginalName()
+            );
+
+            $this->addFlash('success', 'File updated');
+
+            return $this->redirectToRoute('app_admin');
+        }
+
         return $this->render('admin/index.html.twig', [
             'admin' => $this->getUser(),
+            'formPdf' => $formPdf->createView(),
         ]);
     }
     #[Route(path: '/edit', name: 'app_admin_edit', methods: ['GET', 'POST'])]
