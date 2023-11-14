fa5d2f9d758e1134ecafae75e9b232915daa22a0
dgmick <lutmickael@gmail.com>
2023-11-14 16:04:31 -0500

    fix design

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 import $ from 'jquery';
 
 $(document).ready(function() {
-    $('#file-fr, #file-en').on('change', function () {
+    $('#file-fr, #file-en').on('change', () =>  {
         const value = $(this).val();
         if (value === '') return;
 
@@ -10,7 +10,7 @@ $(document).ready(function() {
 
     const sendEmail = $('#send_email');
 
-    sendEmail.on('click', function () {
+    sendEmail.on('click', () =>  {
         const myEmail = $('#myEmail').val();
         const subject = $('#subject').val();
         const comment = $('#comment').val();
@@ -24,43 +24,65 @@ $(document).ready(function() {
         sendEmail.attr('href', href);
     });
 
-    $('#show-infos').on('click', function () {
+    $('#show-infos').on('click', () => {
         $('#pdf, #value').css('display', 'flex').animate({
             height: '100%',
+            marginTop: 0,
+            marginBottom: 0,
         }, 1500)
     });
 
-    $('#about, #skill, #resume, #contact').on('click', function (event) {
+    $('#about, #skill, #resume, #contact').on('click', (event) => {
         const value = event.target.id;
         const about = $('#about-element');
+        const aboutStyle = $('#about-style');
         const skill = $('#skill-element');
+        const skillStyle = $('#skill-style');
         const resume = $('#resume-element');
+        const resumeStyle = $('#resume-style');
         const contact = $('#contact-element');
+        const contactStyle = $('#contact-style');
 
         switch (value) {
             case 'about':
                 about.removeClass('hidden');
+                aboutStyle.removeClass('hidden');
                 skill.addClass('hidden');
+                skillStyle.addClass('hidden');
                 resume.addClass('hidden');
+                resumeStyle.addClass('hidden');
                 contact.addClass('hidden');
+                contactStyle.addClass('hidden');
                 break;
             case 'skill':
                 skill.removeClass('hidden');
+                skillStyle.removeClass('hidden');
                 about.addClass('hidden');
+                aboutStyle.addClass('hidden');
                 resume.addClass('hidden');
+                resumeStyle.addClass('hidden');
                 contact.addClass('hidden');
+                contactStyle.addClass('hidden');
                 break;
             case 'resume':
                 resume.removeClass('hidden');
+                resumeStyle.removeClass('hidden');
                 about.addClass('hidden');
+                aboutStyle.addClass('hidden');
                 skill.addClass('hidden');
+                skillStyle.addClass('hidden');
                 contact.addClass('hidden');
+                contactStyle.addClass('hidden');
                 break;
             case 'contact':
                 contact.removeClass('hidden');
+                contactStyle.removeClass('hidden');
                 about.addClass('hidden');
+                aboutStyle.addClass('hidden');
                 skill.addClass('hidden');
+                skillStyle.addClass('hidden');
                 resume.addClass('hidden');
+                resumeStyle.addClass('hidden');
                 break;
         }
     });
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -40,7 +40,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(length: 50)]
     private ?string $localisation = null;
 
-    #[ORM\Column(length: 10)]
+    #[ORM\Column(length: 50)]
     private ?string $phoneNumber = null;
 
     #[ORM\Column(length: 50)]
